7614821134da69f2355f74cc2f75a65687771f76
Ahmed Aburady <accounts@aburady.com>
2025-12-25 20:55:30 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from typing import Annotated, List
+from langgraph.graph import StateGraph, START, END
+from langgraph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = init_chat_model("gpt-4.1-mini")
